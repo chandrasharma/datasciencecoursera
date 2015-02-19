147ecfda89fe5e51c8387c0426a2e16c34084b5a
Chandramouli <csharma@altair.com>
2015-02-19 13:49:02 -0500

    Assignment-3_Part-1

--- best.R
+++ best.R
@@ -1,3 +1,12 @@
+#Write a function called best that take two arguments: the 2-character abbreviated name of a state and an
+#outcome name. The function reads the outcome-of-care-measures.csv le and returns a character vector
+#with the name of the hospital that has the best (i.e. lowest) 30-day mortality for the specied outcome
+#in that state. The hospital name is the name provided in the Hospital.Name variable. The outcomes can
+#be one of \heart attack", \heart failure", or \pneumonia". Hospitals that do not have data on a particular
+#outcome should be excluded from the set of hospitals when deciding the rankings.
+#Handling ties. If there is a tie for the best hospital for a given outcome, then the hospital names should
+#be sorted in alphabetical order and the rst hospital in that set
+
 best <- function(state, outcome){
   
   dir <- getwd()  
