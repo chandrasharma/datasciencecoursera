54be6ca3b7716c1a5e62cadfe057e23ac971416c
Chandramouli <csharma@altair.com>
2015-02-19 13:37:34 -0500

    Assignment-1_part-1

--- Pollutantmean.R
+++ Pollutantmean.R
@@ -1,3 +1,9 @@
+#Write a function named 'pollutantmean' that calculates the mean of a pollutant (sulfate or nitrate)
+#across a specified list of monitors. The function 'pollutantmean' takes three arguments: 'directory',
+#'pollutant', and 'id'. Given a vector monitor ID numbers, 'pollutantmean' reads that monitors'
+#particulate matter data from the directory specified in the 'directory' argument and returns the mean of
+#the pollutant across all of the monitors, ignoring any missing values coded as NA.
+
 pollutantmean <- function(directory, pollutant, id = 1:332){
 
 dir <- getwd()
@@ -5,8 +11,6 @@ dir1 <- paste(dir, "specdata", sep="/")
 allFiles <- list.files(dir1) 
 fileRange <- allFiles[id]
 fileList <- paste(dir1, fileRange, sep="/")
-  
-print(fileList)
 
                 sulfateCur<-NULL
                 sulfate.apend<-NULL
