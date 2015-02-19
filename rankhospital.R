71db40e7a5358bfc0d458b838682fd4c4862204b
Chandramouli <csharma@altair.com>
2015-02-19 13:49:41 -0500

    Assignment-3_Part-2

--- rankhospital.R
+++ rankhospital.R
@@ -1,6 +1,19 @@
-rankhospital <- function(state, outcome, num){
-  
+#Write a function called rankhospital that takes three arguments: the 2-character abbreviated name of a
+#state (state), an outcome (outcome), and the ranking of a hospital in that state for that outcome (num).
+#The function reads the outcome-of-care-measures.csv le and returns a character vector with the name
+#of the hospital that has the ranking specied by the num argument. For example, the call
+#rankhospital("MD", "heart failure", 5)
+#would return a character vector containing the name of the hospital with the 5th lowest 30-day death rate
+#for heart failure. The num argument can take values \best", \worst", or an integer indicating the ranking
+#(smaller numbers are better). If the number given by num is larger than the number of hospitals in that
+#state, then the function should return NA. Hospitals that do not have data on a particular outcome should
+#be excluded from the set of hospitals when deciding the rankings.
+#Handling ties. It may occur that multiple hospitals have the same 30-day mortality rate for a given cause
+#of death. In those cases ties should be broken by using the hospital name. For example, in Texas (\TX"),
+#the hospitals with lowest 30-day mortality rate for heart failure are shown here.
 
+
+rankhospital <- function(state, outcome, num){
 
   dir <- getwd()  
   dir1 <- paste(dir, "outcome-of-care-measures.csv", sep="/")
