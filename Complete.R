3f5fec6143a9c538b090f6a0c44d0b83e119f31e
Chandramouli <csharma@altair.com>
2015-02-19 13:38:46 -0500

    Assignment-1_part-2

--- Complete.R
+++ Complete.R
@@ -1,51 +1,37 @@
+#Write a function that reads a directory full of files and reports the number of completely observed
+#cases in each data file. The function should return a data frame where the first column is the name of
+#the file and the second column is the number of complete cases.
+
 complete <- function(directory,id = 1:332){
-  ##pollutant <- "sulfate"  
-  ##print(pollutant)
-  idFrame <- data.frame("MonitorID" = id)
-  ## print (id)
+ 
+  #Read individual files in a directory containing hundreds of excel files  
+  idFrame <- data.frame("MonitorID" = id) 
   dir <- getwd()
-  ##print(dir)
-  
   dir1 <- paste(dir, "specdata", sep="/")
-
-  allFiles <- list.files(dir1)
-  ##print(allFiles)
+  allFiles <- list.files(dir1)  
   fileRange <- allFiles[id]
-  ##fileRange <- paste
-  ##print (fileRange)
-
   fileList <- paste(dir1, fileRange, sep="/")
   
-  ##print(check)
+  #initialize variables
   nobs1 <- NULL
   id2<-0
   id3 = NULL
   nobs2 = NULL
   
+  
   for(i in 1:length(id)){
-    
-    ##print(paste(dir1, fileRange, sep="/"))
-    
+
     data <- read.csv(fileList[i])
-    ##print (data)  
-    ##print (fileRange[i])
-    
-    strip <- data[,c(2,3,4)]
-    ##print(strip)
-    sulfatenitrateonly <- strip[complete.cases(strip),]
-    ##print(sulfatenitrateonly) 
-    nobss <- nrow(sulfatenitrateonly)
-    
+    strip <- data[,c(2,3,4)] 
+    sulfatenitrateonly <- strip[complete.cases(strip),] 
+    nobss <- nrow(sulfatenitrateonly)    
     nobs1 <- append(nobs1, nobss)
     id2<-id2+1
-    id3 <- append(id3, id2)
-    
+    id3 <- append(id3, id2)    
     
   }
- ## print (nobs1)
-  ##print(id3)
-  
- nobs2 <- data.frame("id"=id3, "nobs"=nobs1)
+
+  nobs2 <- data.frame("id"=id3, "nobs"=nobs1)
   return(nobs2)
 }
     
